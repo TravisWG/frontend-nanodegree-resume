73c642f4d60c2896dd70603227639c04d92ba67c
Travis Gagnon <traviswgagnon@gmail.com>
2016-04-10 14:40:03 -0500

    Update js\resumeBuilder.js with work, education, bio, and project info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,68 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	"name" : "Travis Gagnon",
+	"role" : "Front End Web Developer",
+	"contacts": {
+		"email" : "traviswgagnon@gmail.com",
+		"mobile" : "218-556-8313",
+		"github" : "TravisWG"
+	},
+	"skills" : ["HTML", "CSS", "JavaScript", "Communication", "Work Ethic"],
+	"pictureURL" : "images/me.jpg",
+	"welcomeMessage" : "Hello!  My name is Travis and I am an aspiring Front End Web Developer."
+}
+
+var work = {
+	"jobs" : [
+		{	
+		"position" : "CSPD Tech II",
+		"employer" : "Sanford Health",
+		"dates" : "July 2014 to Current",
+		"city" : "Fargo"
+		}
+	]
+}
+
+var education = {
+	"schools" : [
+		{
+		"name" : "Bemidji State University",
+		"city" : "Bemidji, MN",
+		"degree" : "B.S.",
+		"major" : "Psychology",
+		"minor" : "Philosophy"
+		},
+		{
+		"name" : "North Dakota State University",
+		"city" : "Fargo, MN",
+		"degree" : "Non-degree",
+		"major" : "Pre-Physical Therapy"		
+		}
+	],	
+	
+	"onlineCourses" : [
+		{
+		"school" : "Udacity",
+		"dates" : "May 2015 - October 2015",
+		"title" : "Intro to Programming Nanodegree",
+		"url" : "https://www.udacity.com/course/intro-to-programming-nanodegree--nd000"
+		},
+		{
+		"school" : "Udacity",
+		"city" : "February 2015 to Current",
+		"title" : "Front End Web Development Nanodegree",
+		"url" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+		}
+	]
+}
+
+var projects = {
+	"projects" : [
+		{
+		"title" : "Intro to Programming Nanodegree Notes Webpage",
+		"dates" : "May 2015 - October 2015",
+		"description" : "Website built using skills learned during Intro to Programming Nanodegree. Content is primarily notes taken during the course. The site is hosted using Google App Engine and features use of HTML, CSS, (minor) JavaScript, and Python.",
+		"image" : "imageNotHere.jpg"
+		}
+	]
+}
+
